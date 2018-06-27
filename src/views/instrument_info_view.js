9fbe0bb7bf6387f3ed6a6c8b8b07b7d30dacb77d
Andy Laughlin <atl11384@gmail.com>
2018-06-27 09:49:32 +0100

    List extension working

--- src/views/instrument_info_view.js
+++ src/views/instrument_info_view.js
@@ -14,14 +14,27 @@ InstrumentInfoView.prototype.displayEvent = function () {
 InstrumentInfoView.prototype.render = function (instrumentFamily) {
   const infoParagraph = document.createElement('p');
   const infoParagraph2 = document.createElement('p');
-  const infoParagraph3 = document.createElement('p');
   infoParagraph.textContent = 'The instrument family is ' + instrumentFamily.name;
   infoParagraph2.textContent = instrumentFamily.description;
-  infoParagraph3.textContent = 'Some examples of instruments in this family are ' + instrumentFamily.instruments;
+  const instrumentList = this.createList(instrumentFamily.instruments);
+  console.log(instrumentList);
+
   this.container.innerHTML = '';
+
   this.container.appendChild(infoParagraph);
   this.container.appendChild(infoParagraph2);
-  this.container.appendChild(infoParagraph3);
+  this.container.appendChild(instrumentList);
+};
+
+InstrumentInfoView.prototype.createList = function (instrumentFamily) {
+  const instrumentList = document.createElement('ul');
+
+  for (let instrument of instrumentFamily) {
+    const listItem = document.createElement('li');
+    listItem.textContent = instrument;
+    instrumentList.appendChild(listItem);
+  }
+  return instrumentList;
 }
 
 module.exports = InstrumentInfoView;
